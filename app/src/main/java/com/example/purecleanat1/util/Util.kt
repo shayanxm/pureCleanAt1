5c2c408332cd46f080ecd49aacf1757f7a1c8a9d
shayan <shayanemoradi@gmail.com>
2023-05-13 23:56:41 +0200

    working

---
+++ app/src/main/java/com/example/purecleanat1/util/Util.kt
@@ -0,0 +1,30 @@
+package com.example.purecleanat1.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.purecleanat1.R
+
+fun getProgressDrawable(context: Context):CircularProgressDrawable{
+return CircularProgressDrawable(context).apply {
+    strokeWidth=10f
+    centerRadius= 50f
+    start()
+}
+
+
+}
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options= RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher_round)
+
+    Glide.with(this.context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+
+
+}
--- app/src/main/java/com/example/purecleanat1/view/CountryListAdapter.kt
+++ app/src/main/java/com/example/purecleanat1/view/CountryListAdapter.kt
@@ -4,12 +4,18 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.purecleanat1.Country
 import com.example.purecleanat1.R
+import com.example.purecleanat1.util.*
+
 
 class CountryListAdapter(var countires: ArrayList<Country>) :
     RecyclerView.Adapter<CountryListAdapter.Viewholder>() {
@@ -21,10 +27,19 @@ class CountryListAdapter(var countires: ArrayList<Country>) :
 
 
     class Viewholder(view: View) : RecyclerView.ViewHolder(view) {
+        private val imageView=view.findViewById<ImageView>(R.id.imageView)
+        private val countryCapital=view.findViewById<TextView>(R.id.capital)
+        private val progressDrawable=getProgressDrawable(view.context)
    //  val countryName =view.name
         val countryName =view.findViewById<TextView>(R.id.name)
         fun bind(country: Country) {
        countryName.text=country.countryName
+            countryCapital.text=country.capital
+
+            imageView.loadImage(country.flag,progressDrawable)
+
+
+
 
         }
 
@@ -50,3 +65,18 @@ class CountryListAdapter(var countires: ArrayList<Country>) :
     }
 
 }
+
+// fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+//     val options= RequestOptions()
+//         .placeholder(progressDrawable)
+//         .error(R.mipmap.ic_launcher_round)
+//
+//     Glide.with(this.context)
+//         .setDefaultRequestOptions(options)
+//         .load(uri)
+//         .into(this)
+//
+//
+//}
+
+
